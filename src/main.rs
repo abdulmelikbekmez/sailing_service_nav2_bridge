8c543bdf216ec85c5fff8bcd7cf37be4f480c311
abdulmelikbekmez <bekmez16@itu.edu.tr>
2023-09-04 08:03:01 +0000

    action client implemented

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ struct DataFormat {
 }
 
 use futures::StreamExt;
+use r2r::nav2_msgs::action::NavigateThroughPoses;
 use r2r::{geometry_msgs::msg::PoseStamped, robot_localization::srv::FromLL};
 use serde::{Deserialize, Serialize};
 
@@ -16,6 +17,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = node.create_client::<FromLL::Service>("/fromLL")?;
     let waiting = node.is_available(&client)?;
 
+    let action_client =
+        node.create_action_client::<NavigateThroughPoses::Action>("/NavigateThroughPoses")?;
+
+    let action_waiting = node.is_available(&action_client)?;
+
     let mut sub_gps =
         node.subscribe::<r2r::std_msgs::msg::String>("gps_waypoints", r2r::QosProfile::default())?;
 
@@ -24,6 +30,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         waiting.await.unwrap();
         println!("service available.");
 
+        println!("waiting for action...");
+        action_waiting.await.unwrap();
+        println!("action available.");
+
         while let Some(msg) = sub_gps.next().await {
             println!("message received from publisher {:?}", msg.data);
             let Ok(data) = serde_json::from_str::<DataFormat>(msg.data.as_str()) else {
@@ -31,7 +41,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 continue;
             };
 
-            let mut goal_poses = vec![];
+            let mut poses = vec![];
 
             for i in 0..data.size {
                 let mut ll_point = r2r::geographic_msgs::msg::GeoPoint::default();
@@ -46,11 +56,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     // pose.header.stamp
                     pose.pose.position.x = resp.map_point.x;
                     pose.pose.position.y = resp.map_point.y;
-                    goal_poses.push(pose);
+                    poses.push(pose);
                 }
             }
 
-            println!("goal poses => {:?}", goal_poses)
+            println!("goal poses => {:?}", poses);
+            let tmp = NavigateThroughPoses::Goal {
+                poses,
+                ..Default::default()
+            };
+
+            let (goal, result, feedback) =
+                action_client.send_goal_request(tmp).unwrap().await.unwrap();
+            let (goal_status, res) = result.await.unwrap();
+            println!("goal_status => {:?}", goal_status);
         }
     });
 
